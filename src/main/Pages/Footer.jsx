192eb8e99200393fad698480cf327e61bf46bbf3
Prajwal Raut <77888068+prajwal-raut@users.noreply.github.com>
2021-12-22 22:41:29 +0530

    version 1.0.1
    console clear

--- src/main/Pages/Footer.jsx
+++ src/main/Pages/Footer.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Footer = () => {
   return (
     <>
-      <footer class="py-3 bg-dark">
-        <div class="container">
-          <p class="m-0 text-center text-white">
+      <footer className="py-3 bg-dark">
+        <div className="container">
+          <p className="m-0 text-center text-white">
             &copy; 2022 - All rights reserved - prajwalraut.com
           </p>
         </div>
